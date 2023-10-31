4d02a08d95b90e37ee1af44ec400a10f3aa47b8e
Adamsidy <isahadamsd@gmail.comgit config --global user.email isahadamsd@gmail.com>
2023-10-31 03:18:06 -0700

    check code

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * *_isupper - checks for  uppercase characters
+ * *_isupper - checks for uppercase characters
  * @c: char
  * Return: 0 or 1
  */
@@ -9,7 +9,7 @@
 int _isupper(int c)
 {
 	int i;
-	for  (i = 'A'; i <= 'Z'; i++)
+	for (i = 'A'; i <= 'Z'; i++)
 	{
 		if (i == c)
 		{
